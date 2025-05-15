7046d6a05d49903700c4e90405759ef407daa60d
Akil-Ansari-cmd <ansariakil0123@gmail.com>
2025-05-15 13:31:00 +0530

    Another Commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,4 @@
-import { useState } from 'react'
 import Weather from './Weather'
-import Blinkit from './Blinkit'
-import Practice1 from './Practices/Practice1'
-import Practice2 from './Practices/Practice2'
-import Practice3 from './Practices/Practice3'
-import Practice4 from './Practices/Practice4'
-import { PracticeApi } from '../Services/Practice'
-import Practice5 from './Practices/Practice5'
 
 function App() {
 
@@ -15,12 +7,6 @@ function App() {
   return (
     <div>
       <Weather/>
-      {/* <Blinkit/> */}
-      {/* <Practice1/> */}
-      {/* <Practice2/>
-      <Practice3/>
-      <Practice4/>
-      <Practice5/> */}
     </div>
   )
 }
